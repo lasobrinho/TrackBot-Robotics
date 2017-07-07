3758b6261caef7cde7adcd3bfa17b4b3b1691bee
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2017-07-07 18:01:40 -0300

    Fixing problem when getting webcam_ip parameter from URL

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import camera_processing
 import subprocess
 import atexit
+import time
 
 app = Flask(__name__)
+webcam_process = None
 webcam_ip = ''
 
 @app.route('/')
 def index():
-	webcam_ip = request.arg.get('webcam_ip')
-	webcam_process = subprocess.Popen(['./prepare-videochat.sh', webcam_ip], shell=True)
+	global webcam_ip
+	webcam_ip = str(request.args.get('webcam_ip'))
+	# webcam_ip = '192.168.0.13'
+	# print()
+	# print(str(webcam_ip))
+	# print()
+	# global webcam_process
+	webcam_process = subprocess.Popen(['./prepare-videochat.sh {}'.format(webcam_ip)], shell=True)
+	time.sleep(5)
 	return render_template('index.html')
 
 def gen():
+	global webcam_ip
 	while True:
 		frame = camera_processing.process_video(ip=webcam_ip)
 		yield (b'--frame\r\n'
@@ -26,7 +36,8 @@ def video_feed():
 					mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def finish_webcam_process():
-	print('finishing page')
+	print('finishing subprocesses')
+	global webcam_process
 	webcam_process.communicate(input='\n')
 	webcam_process.terminate()
 
