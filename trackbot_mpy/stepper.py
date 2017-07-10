0f1740cc27ad2d18580aa73b10caf172cdb762d1
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2017-07-10 00:00:33 -0300

    Adding Stepper class to control 4-wire stepper motors in MicroPython

---
+++ trackbot_mpy/stepper.py
@@ -0,0 +1,61 @@
+import machine
+import time
+
+class Stepper:
+
+	def __init__(self, numberOfSteps, pins):
+		self.pin1 = machine.Pin(pins[0], machine.Pin.OUT)
+		self.pin2 = machine.Pin(pins[1], machine.Pin.OUT)
+		self.pin3 = machine.Pin(pins[2], machine.Pin.OUT)
+		self.pin4 = machine.Pin(pins[3], machine.Pin.OUT)
+		self.stepNumber = 0
+		self.direction = 0
+		self.numberOfSteps = numberOfSteps
+		self.stepDelay = 0
+
+	def setSpeed(self, newSpeed):
+		self.stepDelay = 60.0 / self.numberOfSteps / newSpeed
+
+	def stepMotor(self, thisStep):
+		if thisStep == 0:
+			self.pin1.value(1)
+			self.pin2.value(0)
+			self.pin3.value(1)
+			self.pin4.value(0)
+		if thisStep == 1:
+			self.pin1.value(0)
+			self.pin2.value(1)
+			self.pin3.value(1)
+			self.pin4.value(0)
+		if thisStep == 2:
+			self.pin1.value(0)
+			self.pin2.value(1)
+			self.pin3.value(0)
+			self.pin4.value(1)
+		if thisStep == 3:
+			self.pin1.value(1)
+			self.pin2.value(0)
+			self.pin3.value(0)
+			self.pin4.value(1)
+
+	def step(self, stepsToMove):
+		stepsLeft = abs(stepsToMove)
+
+		if stepsToMove > 0:
+			self.direction = 1
+		if stepsToMove < 0:
+			self.direction = 0
+
+		while stepsLeft > 0:
+			if self.direction == 1:
+				self.stepNumber += 1
+				if self.stepNumber == self.numberOfSteps:
+					self.stepNumber = 0
+			else:
+				if self.stepNumber == 0:
+					self.stepNumber = self.numberOfSteps
+				self.stepNumber -= 1
+			stepsLeft -= 1
+
+			self.stepMotor(self.stepNumber % 4)
+			time.sleep(self.stepDelay)
